f3f4a634a3253bd270fb2110da8147660b164b2b
EzicStar <50255924+EzicStar@users.noreply.github.com>
2023-05-15 16:35:16 -0300

    Make plot by epoch
    Make acc and loss plots by epoch instead of step.

--- classification/train_v3/train.py
+++ classification/train_v3/train.py
@@ -39,13 +39,12 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 writer = SummaryWriter()
 
 # Training function.
-def train(model, trainloader, optimizer, criterion, epoch, writer):
+def train(model, trainloader, optimizer, criterion):
     model.train()
     print('Training')
     train_running_loss = 0.0
     train_running_correct = 0
     counter = 0
-    total_step = len(trainloader)
 
     for i, data in tqdm(enumerate(trainloader), total=len(trainloader)):
         counter += 1
@@ -69,30 +68,17 @@ def train(model, trainloader, optimizer, criterion, epoch, writer):
 
         # Calculate the accuracy.
         _, preds = torch.max(outputs.data, 1)
-        train_total += labels.size(0)
         train_running_correct += (preds == labels).sum().item()
-
-        # Print statistics
-        train_loss += loss.item() * image.size(0)
-        train_loss = train_loss / train_total
-        train_acc = 100 * train_running_correct / train_total
-
-        # Write loss and accuracy to Tensorboard
-        writer.add_scalar('Loss/train', train_loss, epoch * total_step + i)
-        writer.add_scalar('Accuracy/train', train_acc, epoch * total_step + i)
-
-        if i == 0 or (i+1) % 10 == 0:
-            print(f'Epoch [{epoch+1}], Step [{i+1}], Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}')
-            #print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.2f}')
     
     # Loss and accuracy for the complete epoch.
     epoch_loss = train_running_loss / counter
-    epoch_acc = 100. * (train_running_correct / len(trainloader.dataset))
+    epoch_acc = train_running_correct / len(trainloader.dataset)
+
     return epoch_loss, epoch_acc
 
 
 # Validation function.
-def validate(model, testloader, criterion, epoch, writer):
+def validate(model, testloader, criterion):
     model.eval()
     print('Validation')
     valid_running_loss = 0.0
@@ -115,45 +101,24 @@ def validate(model, testloader, criterion, epoch, writer):
 
             # Calculate the accuracy.
             _, preds = torch.max(outputs.data, 1)
-            valid_total += labels.size(0)
             valid_running_correct += (preds == labels).sum().item()
-
-            # Print statistics
-            valid_loss += loss.item() * image.size(0)
-        
-        valid_loss = valid_loss / valid_total
-        valid_acc = 100 * valid_running_correct / valid_total
-
-        #print(f'Epoch [{epoch+1}/{num_epochs}], Validation Loss: {valid_loss:.4f}, Validation Acc: {valid_acc:.2f}')
-        print(f'Epoch [{epoch+1}], Validation Loss: {valid_loss:.4f}, Validation Acc: {valid_acc:.2f}')
-
-        # Write loss and accuracy to Tensorboard
-        writer.add_scalar('Loss/valid', valid_loss, epoch)
-        writer.add_scalar('Accuracy/valid', valid_acc, epoch)
 
         # Save best model based on validation accuracy
         if valid_acc > best_valid_acc:
-            torch.save(model.state_dict(), 'resnet_model.pt')
+            torch.save(model.state_dict(), 'model.pt')
             best_valid_acc = valid_acc
-
-    # Append losses and accuracies for plotting
-    #train_losses.append(train_loss)
-    #valid_losses.append(valid_loss)
-    #train_accs.append(train_acc)
-    #valid_accs.append(valid_acc)
-        
+     
     # Loss and accuracy for the complete epoch.
     epoch_loss = valid_running_loss / counter
-    epoch_acc = 100. * (valid_running_correct / len(testloader.dataset))
+    epoch_acc = valid_running_correct / len(testloader.dataset)
     return epoch_loss, epoch_acc
 
 
 # Test function.
-def test(model, testloader, criterion, epoch, writer):
-    model.load_state_dict(torch.load('resnet_model.pt'))
+def test(model, testloader, criterion):
+    model.load_state_dict(torch.load('model.pt'))
     model.eval()
     print('Test')
-    test_loss = 0
     test_running_loss = 0.0
     test_running_correct = 0
     counter = 0
@@ -174,29 +139,17 @@ def test(model, testloader, criterion, epoch, writer):
             loss = criterion(outputs, labels)
             test_running_loss += loss.item()
 
-
             # Calculate the accuracy.
             _, preds = torch.max(outputs.data, 1)
-            test_total += labels.size(0)
             test_running_correct += (preds == labels).sum().item()
 
             # Append predictions and targets for classification report and confusion matrix
             predictions.append(preds)
             targets.append(labels)
-
-            # Print statistics
-            test_loss += loss.item() * image.size(0)
-        test_loss = test_loss / test_total
-        test_acc = 100 * test_running_correct / test_total
-        print(f'Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}')
-
-        # Write loss and accuracy to Tensorboard
-        writer.add_scalar('Loss/test', test_loss, args['epochs'])
-        writer.add_scalar('Accuracy/test', test_acc, args['epochs'])
     
     # Loss and accuracy for the complete epoch.
     epoch_loss = test_running_loss / counter
-    epoch_acc = 100. * (test_running_correct / len(testloader.dataset))
+    epoch_acc = test_running_correct / len(testloader.dataset)
     return epoch_loss, epoch_acc, predictions, targets
 
 
@@ -265,16 +218,21 @@ if __name__ == '__main__':
     for epoch in range(epochs):
         print(f"[INFO]: Epoch {epoch+1} of {epochs}")
         train_epoch_loss, train_epoch_acc = train(model, train_loader, 
-                                                optimizer, criterion, epoch, writer)
+                                                optimizer, criterion)
         valid_epoch_loss, valid_epoch_acc = validate(model, valid_loader,  
-                                                    criterion, epoch, writer)
+                                                    criterion)
         
-        
-        
+        # Write loss and accuracy to Tensorboard
+        writer.add_scalar('Loss/train', train_loss, epoch)
+        writer.add_scalar('Accuracy/train', train_acc, epoch)
+        writer.add_scalar('Loss/valid', valid_loss, epoch)
+        writer.add_scalar('Accuracy/valid', valid_acc, epoch)
+      
         train_loss.append(train_epoch_loss)
         valid_loss.append(valid_epoch_loss)
         train_acc.append(train_epoch_acc)
         valid_acc.append(valid_epoch_acc)
+
         print(f"Training loss: {train_epoch_loss:.3f}, training acc: {train_epoch_acc:.3f}")
         print(f"Validation loss: {valid_epoch_loss:.3f}, validation acc: {valid_epoch_acc:.3f}")
         print('-'*50)
@@ -282,10 +240,14 @@ if __name__ == '__main__':
     
     test_loss, test_acc, predictions, targets = test(model, test_loader,  
                                 criterion, epoch, writer)
-    print(f"Validation loss: {valid_epoch_loss:.3f}, validation acc: {valid_epoch_acc:.3f}")
-
-    metricas(predictions, targets, writer, dataset_train)
     
+    # Write loss and accuracy to Tensorboard
+    writer.add_scalar('Loss/test', test_loss, args['epochs'])
+    writer.add_scalar('Accuracy/test', test_acc, args['epochs'])
+
+    print(f'Test Loss: {test_loss:.4f}, Test Acc: {test_acc:.2f}')
+
+    metricas(predictions, targets, writer, dataset_train)  
     
     # Save the trained model weights.
     save_model(epochs, model, optimizer, criterion, args['pretrained'])
