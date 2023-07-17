d8cd6fdb81c2d9c74f5e12a1a1d1e1168b349cdf
EzicStar <50255924+EzicStar@users.noreply.github.com>
2023-07-17 19:22:13 -0300

    Minor Model builder changes

--- classification/train_v3/model.py
+++ classification/train_v3/model.py
@@ -4,17 +4,22 @@ import torch.nn as nn
 
 def build_model(model = 'efficientnetb0',pretrained=True, fine_tune=True, num_classes=6):
     num_features = 0
+    weights = None
     if pretrained:
         print('[INFO]: Loading pre-trained weights')
+        if model == 'efficientnetb0':
+            weights = models.EfficientNet_B0_Weights.DEFAULT
+        elif model == 'resnet18':
+            weights = models.ResNet18_Weights.DEFAULT
     else:
         print('[INFO]: Not loading pre-trained weights')
 
     # Choose model
     if model == 'efficientnetb0':
-        model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
+        model = models.efficientnet_b0(weights=weights)
         num_features = 1280
     elif model == 'resnet18':
-        model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        model = models.resnet18(weights=weights)
         num_features = 512
     else:
         raise AttributeError('Invalid model name')
